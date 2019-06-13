7b226f03ad9e6e2c391353fc88e0b43b2dc2a208
hendisantika <hendisantika@yahoo.co.id>
2019-06-14 04:17:57 +0700

    #2 - Add RestTemplate as Bean

--- src/main/java/com/hendisantika/springbootconcurrencysample/SpringbootConcurrencySampleApplication.java
+++ src/main/java/com/hendisantika/springbootconcurrencysample/SpringbootConcurrencySampleApplication.java
@@ -2,9 +2,15 @@ package com.hendisantika.springbootconcurrencysample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringbootConcurrencySampleApplication {
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootConcurrencySampleApplication.class, args);
