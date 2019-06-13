afe0eb95706d30dd75ab2770c40102e53ea10766
hendisantika <hendisantika@yahoo.co.id>
2019-06-14 04:19:19 +0700

    #4 - Add MainController

---
+++ src/main/java/com/hendisantika/springbootconcurrencysample/controller/MainController.java
@@ -0,0 +1,25 @@
+package com.hendisantika.springbootconcurrencysample.controller;
+
+import com.hendisantika.springbootconcurrencysample.kotlin.KotlinBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-concurrency-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-06-14
+ * Time: 04:19
+ */
+@RestController
+public class MainController {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Autowired
+    KotlinBean kotlinBean;
+}
