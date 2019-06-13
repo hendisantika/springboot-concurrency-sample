56c99e30296bbdaab6b5fe32492e1e0a36b5bfcd
hendisantika <hendisantika@yahoo.co.id>
2019-06-14 04:17:22 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootconcurrencysample/SpringbootConcurrencySampleApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootconcurrencysample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootConcurrencySampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootConcurrencySampleApplication.class, args);
+    }
+
+}
